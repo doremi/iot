94932fbd1f10800a760d2f365474c245af76afd9
Shane Lin <253692+doremi@users.noreply.github.com>
2022-01-07 10:16:24 +0800

    Fix init tools for sudoers
    * It should use the current user name

--- init-boards.sh
+++ init-boards.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-echo "ubuntu ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
+if [ -z "$USER" ]; then
+    echo "No $USER found!"
+    exit 2
+fi
+
+echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers
 echo "alias dir='ls -al'" | sudo tee -a /etc/bash.bashrc
